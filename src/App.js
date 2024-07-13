136ac7a81d844b33e715006b6c6d3abe50076627
RomitJain15 <jainromit15@gmail.com>
2024-07-13 20:21:06 +0530

    Made few changes to Login.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import HolidayCard from './Holidays';
 import SignUp from './SignUp';
 import Login from './Login';
 import Bookings from './Bookings';
 import Profile from './Profile';
 
+function HomeRoute() {
+  const location = useLocation();
+  const isLoggedIn = localStorage.getItem('token') ? true : location.state?.isLoggedIn || false;
+
+  return isLoggedIn ? <HolidayCard /> : <Navigate to="/login" replace />;
+}
+
+
 function App() {
+  const location = useLocation();
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('token') ? true : false);
 
-  useEffect(() => {
-    const handleLoginEvent = () => {
-      setIsLoggedIn(localStorage.getItem('token') ? true : false);
-    };
-
-    // Listen for the loginEvent
-    window.addEventListener('loginEvent', handleLoginEvent);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener('loginEvent', handleLoginEvent);
-    };
-  }, []);
-  
   return (
     <Router>
       <Routes>
-        <Route path="/" element={isLoggedIn ? <HolidayCard /> : <Navigate to="/login" replace />} />
+        <Route path="/" element={<HomeRoute />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/mybookings" element={<Bookings />} />
--- src/Login.js
+++ src/Login.js
@@ -18,7 +18,7 @@ function Login() {
             localStorage.setItem('userName', response.data.user.name);
             localStorage.setItem('userId', response.data.user._id);
             localStorage.setItem('token', response.data.token);
-            navigate('/');
+            navigate('/', { state: { isLoggedIn: true } });
         } else if(response.data.message === "Incorrect Password") {
             alert('Incorrect Password')
         } else {
