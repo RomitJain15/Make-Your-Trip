074cc2bc89a6164751c7189efa2065cd9ede7970
RomitJain15 <jainromit15@gmail.com>
2024-07-13 18:17:58 +0530

    Changed baseURL to backend URL

--- src/axiosWithAuth.js
+++ src/axiosWithAuth.js
@@ -4,7 +4,8 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem('token'); // Retrieve the JWT token from localStorage
 
   return axios.create({
-    baseURL: 'http://localhost:5000', // Set the common URL for all requests
+    // baseURL: 'http://localhost:5000',
+    baseURL: 'make-your-trip.vercel.app',
     headers: {
       Authorization: `Bearer ${token}` // Set the Authorization header with the JWT token
     }
