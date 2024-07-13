b08a645a6e5a937ccd5aacde92974f2c5d5aeda0
RomitJain15 <jainromit15@gmail.com>
2024-07-13 19:47:54 +0530

    Edited CORS in server.js

--- api/server.js
+++ api/server.js
@@ -15,10 +15,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 // Middleware
-app.use(cors({
-  origin: 'https://make-your-trip-ajj9.onrender.com',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
