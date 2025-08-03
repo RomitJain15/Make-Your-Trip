980fd51e2c6bd50b0ee9ccd336982063007f3678
RomitJain15 <133482256+RomitJain15@users.noreply.github.com>
2025-08-03 14:47:34 +0530

    Delete src/samples directory

--- src/samples/policy.js
+++
@@ -1,4 +0,0 @@
-export const policy = {
-  title: "Code of Conduct Policy Sample",
-  description: "  Code of Conduct Policy Sample The code of conduct policy of “Name of the Company” is a legal document that describes a set of rules that refers to ideal characteristics and behavior any employee should obey in the organization. The empl."
-}
